90ae3cd955551d84c2c8a8cba432ef80b5e80d46
MinhbaoGDVN <minhbaom45@gmail.com>
2026-05-05 17:54:48 +0700

    Update readdata.js

--- js/readdata.js
+++ js/readdata.js
@@ -1,1 +1,20 @@
+export async function readData() {
 
+    try {
+
+        const response = await fetch("../databass.json");
+
+        const data = await response.json();
+
+        return data;
+
+    }
+    catch (error) {
+
+        console.error("Read data error:", error);
+
+        return null;
+
+    }
+
+}
