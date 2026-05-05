9f95a7c2855d203d09fe5ed5042d824cb218edb9
MinhbaoGDVN <minhbaom45@gmail.com>
2026-05-05 17:55:12 +0700

    Update randomquestion.js

--- js/randomquestion.js
+++ js/randomquestion.js
@@ -1,1 +1,11 @@
+export function randomQuestion(data) {
 
+    if (!data || data.length === 0) {
+        return null;
+    }
+
+    const randomIndex = Math.floor(Math.random() * data.length);
+
+    return data[randomIndex];
+
+}
