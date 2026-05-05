f45e301dd764371d6eeda3557d218be4027e2076
MinhbaoGDVN <minhbaom45@gmail.com>
2026-05-05 17:56:39 +0700

    Update handle.js

--- js/handle.js
+++ js/handle.js
@@ -1,1 +1,22 @@
+export async function handleQuestion(question) {
 
+    if (!question) {
+        throw new Error("Question empty!");
+    }
+
+    document.getElementById("question").innerText =
+        question.question;
+
+    document.getElementById("A").innerText =
+        question.A;
+
+    document.getElementById("B").innerText =
+        question.B;
+
+    document.getElementById("C").innerText =
+        question.C;
+
+    document.getElementById("D").innerText =
+        question.D;
+
+}
